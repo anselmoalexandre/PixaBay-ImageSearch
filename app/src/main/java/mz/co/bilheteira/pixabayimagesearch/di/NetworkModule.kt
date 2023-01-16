520c6a57d062a463443e93354d2f7acf06d9ce6d
Anselmo Alexandre <anselmo@snappmobile.io>
2023-01-16 15:12:54 +0200

    Created network module and added all needed services

---
+++ app/src/main/java/mz/co/bilheteira/pixabayimagesearch/di/NetworkModule.kt
@@ -0,0 +1,49 @@
+package mz.co.bilheteira.pixabayimagesearch.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.Date
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val BASE_URL: String = "https://pixabay.com/api/"
+
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Provides
+    fun provideOkHttpClientBuilder(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .readTimeout(Integer.MAX_VALUE.toLong(), TimeUnit.MILLISECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+    @Provides
+    fun provideRetrofitBuilder(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit.Builder =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
+
+    @Provides
+    fun provideRetrofit(builder: Retrofit.Builder): Retrofit =
+        builder.build()
+
+    @Provides
+    fun provideMoshi(): Moshi = Moshi.Builder()
+        .add(Date::class.java, Rfc3339DateJsonAdapter())
+        .add(KotlinJsonAdapterFactory())
+        .build()
+}
